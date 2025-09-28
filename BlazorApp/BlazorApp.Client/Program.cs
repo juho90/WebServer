67e08172f0083279d964a9b4b711873f28d2a82e
Juho Han <hanjuho1218@gmail.com>
2025-09-29 00:26:22 +0900

    add: 매칭 페이지 추가

--- BlazorApp/BlazorApp.Client/Program.cs
+++ BlazorApp/BlazorApp.Client/Program.cs
@@ -1,5 +1,13 @@
+﻿using BlazorApp.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped<AccessTokenHandler>();
+builder.Services.AddHttpClient("ApiClient", client =>
+    {
+        client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+    })
+    .AddHttpMessageHandler<AccessTokenHandler>();
 
 await builder.Build().RunAsync();
