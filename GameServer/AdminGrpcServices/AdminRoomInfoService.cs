3262535838a28118582ab6910f4eec0514f946c3
Juho Han <hanjuho1218@gmail.com>
2025-10-10 15:31:52 +0900

    add: 매칭 조회 추가 / 매칭된 유저 정보가 사라지지 않는다 왜지?

--- GameServer/AdminGrpcServices/AdminRoomInfoService.cs
+++ GameServer/AdminGrpcServices/AdminRoomInfoService.cs
@@ -25,13 +25,93 @@ namespace GameServer.AdminGrpcServices
         {
             var queueKey = AdminRoomInfoKeys.MatchingQueueCount(request.Region, request.Capacity);
             var uids = await redis.SortedSetRangeByRankAsync(queueKey, request.Offset, request.Offset + request.Count - 1, Order.Ascending);
-            var reply = new MatchingQueueReply();
+            var reply = new MatchingQueueReply
+            {
+                Region = request.Region,
+                Capacity = request.Capacity
+            };
             foreach (var uid in uids)
             {
                 var enqueuedAt = (long)await redis.HashGetAsync(RoomMatchKeys.MatchingUserMeta(uid!), RoomUserMetaHashKey.EnqueuedAt);
                 reply.MatchingQueues.Add(new MatchingQueue
                 {
                     Uid = uid,
+                    EnqueuedAt = enqueuedAt
+                });
+            }
+            return reply;
+        }
+
+        public override async Task<MatchingUserCountReply> GetMatchingUserCount(MatchingUserCountRequest request, ServerCallContext context)
+        {
+            var endpoint = redis.Multiplexer.GetEndPoints()[0];
+            var server = redis.Multiplexer.GetServer(endpoint);
+            var userKeys = server.Keys(pattern: AdminRoomInfoKeys.MatchingUserPattern());
+            return await Task.FromResult(new MatchingUserCountReply
+            {
+                Count = userKeys.Count()
+            });
+        }
+
+        public override async Task<MatchingUserReply> GetMatchingUser(MatchingUserRequest request, ServerCallContext context)
+        {
+            var endpoint = redis.Multiplexer.GetEndPoints()[0];
+            var server = redis.Multiplexer.GetServer(endpoint);
+            var userKeys = server.Keys(pattern: AdminRoomInfoKeys.MatchingUserPattern())
+                .Skip(request.Offset)
+                .Take(request.Count)
+                .ToArray();
+            var reply = new MatchingUserReply
+            {
+                Region = request.Region,
+                Capacity = request.Capacity
+            };
+            foreach (var userKey in userKeys)
+            {
+                var uid = userKey.ToString().Split(':')[2];
+                var enqueuedAt = (long)await redis.HashGetAsync(RoomMatchKeys.MatchingUserMeta(uid!), RoomUserMetaHashKey.EnqueuedAt);
+                reply.MatchingUsers.Add(new MatchingUser
+                {
+                    Uid = uid,
+                    EnqueuedAt = enqueuedAt
+                });
+            }
+            return reply;
+        }
+
+        public override async Task<MatchingRoomCountReply> GetMatchingRoomCount(MatchingRoomCountRequest request, ServerCallContext context)
+        {
+            var endpoint = redis.Multiplexer.GetEndPoints()[0];
+            var server = redis.Multiplexer.GetServer(endpoint);
+            var roomKeys = server.Keys(pattern: AdminRoomInfoKeys.MatchingRoomPattern());
+            return await Task.FromResult(new MatchingRoomCountReply
+            {
+                Count = roomKeys.Count()
+            });
+        }
+
+        public override async Task<MatchingRoomReply> GetMatchingRoom(MatchingRoomRequest request, ServerCallContext context)
+        {
+            var endpoint = redis.Multiplexer.GetEndPoints()[0];
+            var server = redis.Multiplexer.GetServer(endpoint);
+            var userKeys = server.Keys(pattern: AdminRoomInfoKeys.MatchingRoomPattern())
+                .Skip(request.Offset)
+                .Take(request.Count)
+                .ToArray();
+            var reply = new MatchingRoomReply
+            {
+                Region = request.Region,
+                Capacity = request.Capacity
+            };
+            foreach (var userKey in userKeys)
+            {
+                var uid = userKey.ToString().Split(':')[2];
+                var roomId = await redis.StringGetAsync(RoomMatchKeys.MatchedRoomId(uid!));
+                var enqueuedAt = (long)await redis.HashGetAsync(RoomMatchKeys.MatchingUserMeta(uid!), RoomUserMetaHashKey.EnqueuedAt);
+                reply.MatchingRooms.Add(new MatchingRoom
+                {
+                    Uid = uid,
+                    RoomId = roomId,
                     EnqueuedAt = enqueuedAt
                 });
             }
@@ -54,8 +134,11 @@ namespace GameServer.AdminGrpcServices
             var endpoint = redis.Multiplexer.GetEndPoints()[0];
             var server = redis.Multiplexer.GetServer(endpoint);
             var roomInfoReply = new RoomInfoReply();
-            var roomKeys = server.KeysAsync(pattern: AdminRoomInfoKeys.RoomPattern(), pageOffset: request.Offset, pageSize: request.Count);
-            await foreach (var roomKey in roomKeys)
+            var roomKeys = server.Keys(pattern: AdminRoomInfoKeys.RoomPattern())
+                .Skip(request.Offset)
+                .Take(request.Count)
+                .ToArray();
+            foreach (var roomKey in roomKeys)
             {
                 var json = await redis.StringGetAsync(roomKey);
                 var roomInfo = RoomInfo.Parser.ParseJson(json);
